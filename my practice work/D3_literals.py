f43f220b965d9134b0cc80ca3f2e592d6935b9b8
simran786-QA <smrntester@gmail.com>
2025-11-27 00:01:16 +0530

    Literalb in Python

---
+++ my practice work/D3_literals.py
@@ -0,0 +1,20 @@
+
+a=10
+b=30
+c=70 # number literals
+
+variable_name = literals # literals cannot be changed , automatically untill manually done
+d=2.9 # float literal
+e= "simran" # string literal
+f= 2+d #complex literal
+g = true #boolean literal
+ print(a,b,c,d,e,f,g)
+
+list_lit = [1, 2, 3]          # List literal
+tuple_lit = (4, 5, 6)         # Tuple literal
+dict_lit = {"name": "Simran"} # Dictionary literal
+set_lit = {10, 20, 30}        # Set literal
+
+print(list_lit, tuple_lit, dict_lit, set_lit)
+
+
