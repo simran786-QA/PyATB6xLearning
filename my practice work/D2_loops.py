e880e57b6ff75f2147006c1cc1ca195b1b3fd05c
simran786-QA <smrntester@gmail.com>
2025-12-31 00:01:56 +0530

    for aur while loop ka use karke mul table programs likhe

--- my practice work/D2_loops.py
+++ my practice work/D2_loops.py
@@ -4,14 +4,14 @@ for i in range(10,0,-1): #range(10{start},0{stop},-1{value_for_++/--})
    print(i)
 """
 #🔹 Task 2: Table Till User-Defined Range
-""" num1 =int(input("enter number:"))
+num1 =int(input("enter number:"))
 limit = int(input("enter limit:"))
 for i in range(num1,limit+1):
  print(num1,"X",i,"=",num1*i)
- """
+
 #Task 3: Table Using while Loop
-number = int(input("enter the number:"))
+"""number = int(input("enter the number:"))
 i=1
 while i<=10:
      print(number,"X",i,"=",number*i)
-     i+=1
+     i+=1"""
